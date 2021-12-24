fd3e11ce953ddccad13b97bec0609b2455405535
Kutay Kağan Özen <kutay.kagan.ozenn@gmail.com>
2021-12-25 01:17:00 +0300

    add boilerplate for redux

---
+++ src/state/action-creators/index.js
@@ -0,0 +1,17 @@
+export const deposit = () => {
+    return (dispatch) => {
+
+    }
+}
+
+export const withdraw = () => {
+    return (dispatch) => {
+
+    }
+}
+
+export const multiply = () => {
+    return (dispatch) => {
+
+    }
+}
